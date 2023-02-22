f07c19137e1ede12518cb87403d8565986fa5d19
Mihai <123064601+mikhailgutsu@users.noreply.github.com>
2023-02-22 09:09:37 +0200

    Add files via upload

---
+++ Cpp_Com-Port/Cpp_Com-Port/Cpp_Com-Port.cpp
@@ -0,0 +1,69 @@
+﻿//Header файлы
+#include <windows.h>
+#include <iostream>
+#include "tchar.h"
+using namespace std;
+
+//Объявляем обработчик com порта:
+HANDLE hSerial;
+//Я делаю это глобально, чтобы не заморачиваться с указателями при передаче его в функции.
+
+void ReadCOM();
+
+//Дальше начинаем формировать функцию main :
+int _tmain(int argc, _TCHAR* argv[])
+{
+	LPCTSTR sPortName = L"COM1";
+
+	hSerial = ::CreateFile(sPortName, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+
+    if (hSerial == INVALID_HANDLE_VALUE)
+    {
+        if (GetLastError() == ERROR_FILE_NOT_FOUND)
+        {
+            cout << "serial port does not exist.\n";
+        }
+        cout << "some other error occurred.\n";
+    }
+
+    DCB dcbSerialParams = { 0 };
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    if (!GetCommState(hSerial, &dcbSerialParams))
+    {
+        cout << "getting state error\n";
+    }
+    dcbSerialParams.BaudRate = CBR_9600;
+    dcbSerialParams.ByteSize = 8;
+    dcbSerialParams.StopBits = ONESTOPBIT;
+    dcbSerialParams.Parity = NOPARITY;
+    if (!SetCommState(hSerial, &dcbSerialParams))
+    {
+        cout << "error setting serial port state\n";
+    }
+
+    char data[] = "Hello from C++";  // строка для передачи
+    DWORD dwSize = sizeof(data);   // размер этой строки
+    DWORD dwBytesWritten;    // тут будет количество собственно переданных байт
+
+    BOOL iRet = WriteFile(hSerial, data, dwSize, &dwBytesWritten, NULL);
+
+    cout << dwSize << " Bytes in string. " << dwBytesWritten << " Bytes sended. " << endl;
+
+    while (1)
+    {
+        ReadCOM();
+    }
+    return 0;
+}
+
+void ReadCOM()
+{
+    DWORD iSize;
+    char sReceivedChar;
+    while (true)
+    {
+        ReadFile(hSerial, &sReceivedChar, 1, &iSize, 0);  // получаем 1 байт
+        if (iSize > 0)   // если что-то принято, выводим
+            cout << sReceivedChar;
+    }
+}
